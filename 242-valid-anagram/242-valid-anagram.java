3deb76e60543cf7053374ffdd83cc5352c4a5765
Sakshi Tomar <56171016+sakshi-0809@users.noreply.github.com>
2022-05-15 20:48:42 +0530

    Time: 19 ms (24.51%), Space: 46.5 MB (18.36%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+            return false;
+        
+        HashMap<Character, Integer> frequency = new HashMap<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            frequency.put(s.charAt(i), frequency.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        
+        for(int i = 0; i < t.length(); i++) {
+            frequency.put(t.charAt(i), frequency.getOrDefault(t.charAt(i), 0) - 1);
+            
+            if(frequency.get(t.charAt(i)) < 0)
+                return false;
+        }
+        
+        return true;
+    }
+}
