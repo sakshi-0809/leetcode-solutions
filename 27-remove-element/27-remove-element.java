1d0bccd6cba7816fa8420956252a683dc834e418
Sakshi Tomar <56171016+sakshi-0809@users.noreply.github.com>
2022-05-30 19:45:37 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (98.73%) - LeetHub

--- 27-remove-element/27-remove-element.java
+++ 27-remove-element/27-remove-element.java
@@ -2,6 +2,10 @@ class Solution {
     public int removeElement(int[] nums, int val) {
         int start = 0, end = nums.length - 1;
         
+        // checking for start == end also because element at that index might be equal to val, 
+        // in that case, line number 14 and 15 executes and end reduced by 1 since nums[end] = nums[start] = val
+        // hence we return end + 1 (and not start + 1)
+        
         while(start <= end) {
             if(nums[start] != val)
                 ++start;
