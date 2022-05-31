f68143c6a23aa89eaa155a574ffb9f6284339c81
Sakshi Tomar <56171016+sakshi-0809@users.noreply.github.com>
2022-05-31 21:18:40 +0530

    Time: 1 ms (17.38%), Space: 41.8 MB (30.52%) - LeetHub

---
+++ 198-house-robber/198-house-robber.java
@@ -0,0 +1,30 @@
+class Solution {
+    // memoization using HashMap
+    
+    HashMap<Integer, Integer> memo = new HashMap<>(); 
+    
+    public int helper(int[] nums, int index) {
+        // returning the max amount for an index if it has already been processed before and is stored in the hash map
+        
+        if(memo.containsKey(index))
+            return memo.get(index);
+        
+        if(index == nums.length)
+            return 0;
+        
+        if(index == nums.length - 1)
+            return nums[index];
+        
+        int maxAmount = Math.max(helper(nums, index + 1), nums[index] + helper(nums, index + 2));
+        
+        // memoizing or storing the max amount for a particular index 
+        
+        memo.put(index, maxAmount);
+        
+        return maxAmount;
+    }
+    
+    public int rob(int[] nums) {
+        return helper(nums, 0);
+    }
+}
