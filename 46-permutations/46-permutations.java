00e2fbce3199af2ee80405b7eaaf6e1f6fda2787
Sakshi Tomar <56171016+sakshi-0809@users.noreply.github.com>
2022-05-31 22:39:03 +0530

    Time: 2 ms (66.13%), Space: 44.5 MB (60.72%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,27 +1,27 @@
 class Solution {
     List<List<Integer>> result = new ArrayList<>();   
     
-    public void helper(int[] nums, List<Integer> current, Set<Integer> visited) {
+    public void helper(int[] nums, List<Integer> current, boolean[] visited) {
         if(current.size() == nums.length) {
             result.add(new ArrayList<>(current));
             return;
         }
         
         for(int i = 0; i < nums.length; i++) {
-            if(!visited.contains(nums[i])) {
+            if(!visited[i]) {
                 current.add(nums[i]);
-                visited.add(nums[i]);
+                visited[i] = true;
                 
                 helper(nums, current, visited);
                 
                 current.remove(current.size() - 1);
-                visited.remove(nums[i]);
+                visited[i] = false;
             }
         }
     }
     
     public List<List<Integer>> permute(int[] nums) { 
-        helper(nums, new ArrayList<>(), new HashSet<>());
+        helper(nums, new ArrayList<>(), new boolean[nums.length]);
             
         return result;
     }
