644de844414c19d1822fbb1421875d1e3bc597ab
Sakshi Tomar <56171016+sakshi-0809@users.noreply.github.com>
2022-06-06 21:38:37 +0530

    Time: 22 ms (29.47%), Space: 50.2 MB (58.24%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -2,10 +2,18 @@ class Solution {
      public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> freqMap = new HashMap<>();
         
+         // key in hashmap stores the number, and value stores the frequency of the number
+         
         for(int n : nums) {
             freqMap.put(n, freqMap.getOrDefault(n, 0) +1);
         }
         
+         // x and y are 2 separate objects in the queue, 
+         // compare(y[0], x[0]) means that we want to sort the queue in descending order based on the first element of the object
+         
+         // the object will be an int array, with [0] = value from hashmap i.e. the frequence 
+         // and [1] = key from the hashmap
+         
         PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> (Integer.compare(y[0],x[0])));
         
         for(Map.Entry<Integer, Integer> e : freqMap.entrySet()) {
