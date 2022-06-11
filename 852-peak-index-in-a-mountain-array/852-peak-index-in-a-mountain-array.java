36a5d7f008f71c541573e224e3db192f47c35367
Sakshi Tomar <56171016+sakshi-0809@users.noreply.github.com>
2022-06-12 01:01:30 +0530

    Update 852-peak-index-in-a-mountain-array.java

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -1,23 +1,23 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int start = 1, end = arr.length - 1;
+        int start = 0, end = arr.length - 1;
         
-        while(start <= end) {
+        while(start < end) {
             int mid = start + (end - start) / 2;
-            
-            if((arr[mid - 1] < arr[mid]) && (arr[mid] > arr[mid + 1])) {
-                return mid;
-            }
-            
-            else if((arr[mid - 1] < arr[mid]) && (arr[mid] < arr[mid + 1])) {
+  
+            // this means we are still in the increasing area of the array
+    
+            if(arr[mid] < arr[mid + 1]) {
                 start = mid + 1;
             }
-            
+
+            // this means we are in the decreasing area of the array
+    
             else {
-                end = mid - 1;
+                end = mid;
             }
         }
-        
-        return -1;
+
+        return start;
     }
 }
