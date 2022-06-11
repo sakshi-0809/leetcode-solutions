195c52a7d4c9cb0dfc15676d2c8139892f438455
Sakshi Tomar <56171016+sakshi-0809@users.noreply.github.com>
2022-06-12 00:57:23 +0530

    Update alternate-solution-binary-search.java

--- 852-peak-index-in-a-mountain-array/alternate-solution-binary-search.java
+++ 852-peak-index-in-a-mountain-array/alternate-solution-binary-search.java
@@ -1,21 +1,26 @@
 // Alternate approach using Binary Search - https://www.youtube.com/watch?v=LHfS7bA6dCA
     
-int start = 0, end = arr.length - 1;
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 1, end = arr.length - 1;
         
-while(start < end) {
-  int mid = start + (end - start) / 2;
-  
-  // this means we are still in the increasing area of the array
-    
-  if(arr[mid] < arr[mid + 1]) {
-    start = mid + 1
-  }
-
-  // this means we are in the decreasing area of the array
-    
-  else {
-    end = mid;
-  }
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if((arr[mid - 1] < arr[mid]) && (arr[mid] > arr[mid + 1])) {
+                return mid;
+            }
+            
+            else if((arr[mid - 1] < arr[mid]) && (arr[mid] < arr[mid + 1])) {
+                start = mid + 1;
+            }
+            
+            else {
+                end = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
 }
-
-return start;
