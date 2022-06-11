b9b6eb2d5a7dd87cfc63e4ae66e2bf447b5fdd4a
Sakshi Tomar <56171016+sakshi-0809@users.noreply.github.com>
2022-06-12 00:34:48 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (98.13%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -20,26 +20,4 @@ class Solution {
         
         return -1;
     }
-    
-    // Alternate approach using Binary Search - https://www.youtube.com/watch?v=LHfS7bA6dCA
-    
-    // int start = 0, end = arr.length - 1;
-        
-    // while(start < end) {
-    //    int mid = start + (end - start) / 2;
-
-    // this means we are still in the increasing area of the array
-    
-    //    if(arr[mid] < arr[mid + 1]) {
-    //        start = mid + 1
-    //    }
-
-    // this means we are in the decreasing area of the array
-    
-    //    else {
-    //        end = mid - 1;
-    //    }
-    // }
-
-    // return start;
 }
