723db4db813369e2f71430422f85fa0a38a1e618
Sakshi Tomar <56171016+sakshi-0809@users.noreply.github.com>
2022-06-14 22:27:54 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (87.29%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    HashMap<Integer, Integer> memo = new HashMap<>();
+    
+    public int climbStairs(int n) {
+        if(memo.containsKey(n))
+            return memo.get(n);
+        
+        if(n < 0) 
+            return 0;
+        
+        if(n == 0)
+            return 1;
+        
+        int valFromRecursion;
+        
+        valFromRecursion = climbStairs(n - 1) + climbStairs(n - 2);
+        
+        memo.put(n, valFromRecursion);
+        
+        return memo.get(n);
+    }
+}
